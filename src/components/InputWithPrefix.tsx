f1b7b2183222aafbcee8b42061ff3a6824cf548b
howdyfrom2019 <tjd1emd@naver.com>
2023-04-21 14:22:16 +0900

    Add watch callback for onchange handler

--- src/components/InputWithPrefix.tsx
+++ src/components/InputWithPrefix.tsx
@@ -12,7 +12,7 @@ const InputWithPrefix = ({ prefix, register, error, ...props }: Props) => {
     <div className={'relative flex gap-8 items-center w-full'}>
       <span className={'font-sans font-bold tracking-tight'}>{prefix}</span>
       <input className={'flex-1 outline-none bg-white rounded-sm border-[1px] border-gray-200 focus:shadow-2xl focus:border-blue-500 px-2 py-1'} {...props} {...register} />
-      <p className={'text-red-500 absolute top-0 right-0 -translate-y-1/2'}>{error}</p>
+      <p className={'text-red-500 absolute top-1/2 right-0 -translate-y-1/2'}>{error}</p>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,27 @@ import TextAreaWithPrefix from '@/components/TextAreaWithPrefix';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useEffect } from 'react';
 
 const schema = yup.object({
-  name: yup.string().required('이름은 필수인데요..?').min(2, '이름이 한글자는 좀...'),
+  name: yup.string().required('이름은 필수입력입니다.').min(2, '최소 2글자 이상 입력해주세요'),
+  age: yup.number().required('나이는 필수입력입니다.').min(19, '19세 이상만 가입할 수 있습니다.'),
 });
 
 export default function Home() {
-  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
-  console.log(errors);
+  const { register, watch, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => console.log(value, name, type));
+    return () => subscription.unsubscribe();
+  }, [watch]);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-16">
       <h2 className={'text-4xl font-bold text-center'}>React-Hook-Form + yup은 어떻게 쓰는가?</h2>
-      <form className={'flex flex-col gap-6 bg-white shadow-2xl rounded-md px-4 py-6 w-[380px]'}>
+      <form
+        className={'flex flex-col gap-6 bg-white shadow-2xl rounded-md px-4 py-6 w-[380px]'}
+        onSubmit={handleSubmit((data) => console.log(JSON.stringify(data)))}>
         <InputWithPrefix
           id={'name'}
           prefix={'이름'}
@@ -35,9 +43,13 @@ export default function Home() {
         <InputWithPrefix
           id={'age'}
           prefix={'나이'}
-          register={register('age', { min: 19, max: 120 })} />
+          error={errors.age?.message}
+          defaultValue={19}
+          type={'number'}
+          register={register('age', { min: 19, max: 120 })}
+        />
         <TextAreaWithPrefix id={'irl'} prefix={'자기소개'} />
-        <button className={'rounded-md bg-lime-500 text-white font-bold text-2xl px-6 py-2'}>제출하기</button>
+        <input type={'submit'} value={'제출하기'} className={'rounded-md bg-lime-500 text-white font-bold text-2xl px-6 py-2'} />
       </form>
     </main>
   )
